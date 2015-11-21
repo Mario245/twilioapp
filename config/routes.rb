d991b8c1d0f652e62c83991a0880f3176572e527
Mario245 <mario.peart@live.com>
2015-11-20 20:39:30 -0500

    hardcode variables_3

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-	resources :posts
+
 	post '/posts/twilio_create' => 'posts#twilio_create'
 	get '/posts/twilio_create' => 'posts#twilio_create'
- 
+ 	resources :posts
   
   #receive_text'
   resources :smsmsgs
