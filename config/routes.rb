32e0db13eeba0660b121fe82b7ec7dccd22f8ae3
Mario245 <mario.peart@live.com>
2015-11-22 14:28:01 -0500

    add redirect to msgs route

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   resources :tenants
   resources :msgs
   resources :msgs
-	post '/messages/twilio_create' => 'messages#twilio_create'
-	get '/messages/twilio_create' => 'messages#twilio_create'
+	post '/msgs/twilio_create' => 'messages#twilio_create'
+	get '/msges/twilio_create' => 'messages#twilio_create'
 
   resources :messages
 	post '/posts/twilio_create' => 'posts#twilio_create'
